810696386362626eee71b995e60eb83c88bb6c03
BreadOne <bbangwon.chung@gmail.com>
2026-01-03 14:39:10 +0900

    프로젝트 생성 & ADefenseTower 헤더 추가

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pangaea
+
+Developed with Unreal Engine 5
---
+++ Source/Pangaea/DefenseTower.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DefenseTower.generated.h"
+
+UCLASS()
+class PANGAEA_API ADefenseTower : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADefenseTower();
+
+	int HealthPoints = 100;
+	int ShellDefense = 2;
+	float AttackRange = 15.0f;
+	float ReloadInterval = 1.0f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	int _HealthPoints;
+	float _ReloadCountingDown;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	int GetHealthPoints();
+	bool IsDestroyed();
+	bool CanFire();
+	void Fire();
+	void Hit(int damage);
+
+protected:
+	void DestroyProcess();
+};
